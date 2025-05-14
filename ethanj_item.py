892b8f4577ea9c07a90467baf55b6bf4364909bf
dilutedious <>
2025-05-14 14:57:18 +1000

    Edited?

--- ethanj_item.py
+++ ethanj_item.py
@@ -1,11 +1,25 @@
 # pieces
 
 class Piece:
-    def __init__(self, colour, symbol):
+    def __init__(self, colour, symbols):
         self.Colour = colour
-        self.Symbol = symbol
+        self.Symbol = symbols
         self.HasMoved = False
 
     def get_symbol(self):
         if self.Colour == 'w':
-            
+            return self.Symbol[1]
+        elif self.Colour == 'b':
+            return self.Symbol[0]
+
+    def move_intent(startpos, endpos, capture):
+        return NotImplementedError
+    
+    def slidingpiece():
+        return False
+    
+class Pawn(Piece):
+    def __init__(self, colour, symbols):
+        super().__init__(colour, symbols = "♙♟")
+
+    def 
